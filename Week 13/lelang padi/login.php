4ce9f615489ea81d6769539f178d4f068c9e67f0
Suandika <if321061@students.del.ac.id>
2022-04-26 12:31:22 +0700

    isi comment

---
+++ Week 13/lelang padi/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Website &mdash; Lelang Padi</title>
+  <link rel="stylesheet" href="css/login.css">
+</head>
+  <body>
+  <div class="login-page">
+  <div class="form">
+    <form class="login-form">
+        <h2>Login</h2>
+      <input type="text" placeholder="Email"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Belum punya akun? <a href="register.php">Register here</a></p>
+    </form>
+  </div>
+</div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/jquery-migrate-3.0.1.min.js"></script>
+      <script src="js/jquery-ui.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.stellar.min.js"></script>
+      <script src="js/jquery.countdown.min.js"></script>
+      <script src="js/bootstrap-datepicker.min.js"></script>
+      <script src="js/jquery.easing.1.3.js"></script>
+      <script src="js/aos.js"></script>
+      <script src="js/jquery.fancybox.min.js"></script>
+      <script src="js/jquery.sticky.js"></script>
+      <script src="js/jquery.mb.YTPlayer.min.js"></script>
+      <script src="js/main.js"></script>
+      
+    </body>
+    </html>
---
+++ Week 13/lelang padi/register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Website &mdash; Lelang Padi</title>
+  <link rel="stylesheet" href="css/login.css">
+</head>
+  <body>
+  <div class="login-page">
+  <div class="form">
+    <form class="login-form">
+        <h2>Register Here</h2>
+      <input type="text" placeholder="Nama:Lengkap"/>
+      <input type="password" placeholder="No.Handphone"/>
+      <input type="text" placeholder="No.Rekening"/>
+      <input type="password" placeholder="Email"/>
+      <input type="text" placeholder="Alamat"/>
+      <input type="text" placeholder="Password"/>
+      <input type="password" placeholder="Konfirmasi Password"/>
+      <button>Register Here</button>
+      <p class="message">Sudah punya akun ? <a href="login.php">Register here</a></p>
+    </form>
+  </div>
+</div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/jquery-migrate-3.0.1.min.js"></script>
+      <script src="js/jquery-ui.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.stellar.min.js"></script>
+      <script src="js/jquery.countdown.min.js"></script>
+      <script src="js/bootstrap-datepicker.min.js"></script>
+      <script src="js/jquery.easing.1.3.js"></script>
+      <script src="js/aos.js"></script>
+      <script src="js/jquery.fancybox.min.js"></script>
+      <script src="js/jquery.sticky.js"></script>
+      <script src="js/jquery.mb.YTPlayer.min.js"></script>
+      <script src="js/main.js"></script>
+      
+    </body>
+    </html>
